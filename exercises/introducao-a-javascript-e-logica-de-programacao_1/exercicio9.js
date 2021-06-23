349237823fb6d17820407ffdd49c8c56cfde90e9
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-06-23 19:17:59 -0300

    Adicionado exercicio 9

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio9.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio9.js
@@ -2,11 +2,11 @@ let a;
 let b;
 let c;
 
-a = 7;
-b = 5;
+a = 4;
+b = 6;
 c = 8;
 
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
     console.log(true);
 } else {
     console.log(false);

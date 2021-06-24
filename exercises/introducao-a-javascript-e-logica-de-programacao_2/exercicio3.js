8a17f85bf9ee3e5c60999b0a9e0b7b83fbeedf76
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-06-24 18:09:19 -0300

    Adicionando exercicio 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio3.js
@@ -0,0 +1,21 @@
+/*
+
+Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+Copiar
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let resultado = 0;
+
+for (let indice = 0; indice < numbers.length ; indice += 1) {
+    resultado += numbers[indice];
+}
+
+let media = (resultado / (numbers.length) )
+
+console.log(`A média aritmética dos elementos do Array é ${media}`)

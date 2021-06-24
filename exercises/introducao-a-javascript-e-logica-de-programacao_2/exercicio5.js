0434f53a7b794380561a7806afb11ba434934fad
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-06-24 18:56:27 -0300

    Adicionando exercicio 7 e atualizando exercicio 5 com numeros infinitos negativos e positivos

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5.js
@@ -10,11 +10,13 @@ Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maiorNumero = 0;
+let maiorNumero = Number.NEGATIVE_INFINITY;
 
 for (let i = 0; i < numbers.length ; i += 1 ) {
     if ( numbers[i] > maiorNumero){
         maiorNumero = numbers[i];
+    } else {
+        maiorNumero = maiorNumero
     }
-    console.log(`O maior numero na repetição ${i+1} é ${maiorNumero}`)
+    console.log(`O maior numero na repetição ${i+1} é ${maiorNumero}`);
 }
--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio7.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio7.js
@@ -10,7 +10,7 @@ Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let menorValor;
+let menorValor = Number.POSITIVE_INFINITY;
 
 for (let contador = 0; contador < numbers.length; contador +=1) {
     

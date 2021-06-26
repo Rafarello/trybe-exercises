60497f0a5d3582e09aea506d351afa64f0f7213b
Rafael Moraes da Rocha <rafael1moraes1rocha@gmail.com>
2021-06-26 16:25:20 -0300

    Mais mudanças para ex bonus 1 e 1.2
    Mais mudanças para exercício bônus 1 e 1.2 para funcionarem com números pares também

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio-bonus.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio-bonus.js
@@ -1,6 +1,21 @@
+/*
+
+ Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+
+Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+
+n = 7
+
+   *
+  * *
+ *   *
+*******
+
+*/
+
 let n;
 
-n = 6;
+n = 8;
 
 if (n % 2 !== 0) {
     for (let index = (n - 1); index >= 0; index -= 2) {
@@ -32,4 +47,36 @@ if (n % 2 !== 0) {
 
 } else {
     console.log("Por favor inserir um valor ímpar")
+
+    for (let index = (n); index > 0; index -= 2) {
+        linha = '';
+        for (let i = 1; i <= index; i += 2) {
+            linha += ' ';
+        }
+        let primeiroAst = index + 1;
+        let ultimoAst = (n + 1)
+        if (index === 2) {
+            for (let numeroDeAsteriscos = 1; numeroDeAsteriscos <= n; numeroDeAsteriscos += 1) {
+                if (numeroDeAsteriscos === 1) {
+                    linha += ' '
+                } else {
+                    linha += '*'
+                }
+
+            }
+        } else {
+
+            for (let i = index; i <= n + 2; i += 1) {
+
+                if (i === primeiroAst || i === ultimoAst) {
+
+                    linha += '*';
+
+                } else if (i !== primeiroAst && i <= n + 2) {
+                    linha += ' '
+                }
+            }
+        }
+        console.log(linha);
+    }
 }
--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio-bonus1.2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio-bonus1.2.js
@@ -1,16 +1,54 @@
-let n = 5; // 
-let meioDaMatrix = ( n + 1 ) / 2
-let primeiroAsterisco = meioDaMatrix;
-let ultimoAsterisco = meioDaMatrix;
+/*
 
-for (let index = 1; index <= meioDaMatrix; index += 1, ultimoAsterisco += 1, primeiroAsterisco -= 1) {
-    let linha = '';
-    for ( i = 1; i <= n ; i += 1) {
-        if (i === primeiroAsterisco || i === ultimoAsterisco || index === meioDaMatrix){
-            linha += '*';
-        } else {
-            linha += ' ';
+/*
+
+ Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+
+Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+
+n = 7
+
+   *
+  * *
+ *   *
+*******
+
+*/
+
+// Diferente do proposto, fiz um código que funcionasse com números pares também
+
+let n = 8;
+
+if (n % 2 !== 0) {
+    let meioDaMatrix = (n + 1) / 2
+    let primeiroAsterisco = meioDaMatrix;
+    let ultimoAsterisco = meioDaMatrix;
+
+    for (let index = 1; index <= meioDaMatrix; index += 1, ultimoAsterisco += 1, primeiroAsterisco -= 1) {
+        let linha = '';
+        for (i = 1; i <= n; i += 1) {
+            if (i === primeiroAsterisco || i === ultimoAsterisco || index === meioDaMatrix) {
+                linha += '*';
+            } else {
+                linha += ' ';
+            }
         }
-   }
-    console.log(linha)
+        console.log(linha)
+    }
+} else {
+    let meioDaMatrix = (n / 2)
+    let primeiroAsterisco = meioDaMatrix;
+    let ultimoAsterisco = meioDaMatrix;
+
+    for (let index = 1; index <= meioDaMatrix; index += 1, ultimoAsterisco += 1, primeiroAsterisco -= 1) {
+        let linha = '';
+        for (i = 0; i <= n; i += 1) {
+            if (i === primeiroAsterisco || i === ultimoAsterisco || index === meioDaMatrix) {
+                linha += '*';
+            } else {
+                linha += ' ';
+            }
+        }
+        console.log(linha)
+    }
 }

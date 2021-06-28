61ab5c8bb5326b8e6183df37697c875a4e20190f
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-06-28 19:19:06 -0300

    Adiciona exercicio de objeto e funções

---
+++ Bloco_4/dia_4/function.js
@@ -0,0 +1,15 @@
+function saudacoes(name){
+
+    console.log('Olá ,' + name);
+}
+saudacoes('Rafael');
+
+function fazerSaudacoes(name, age) {
+    return `E aí ${name}? Tudo bem com você? Você tem ${age} anos`;
+}
+console.log(fazerSaudacoes('Rafael', 27));
+
+function soma(num1, num2) {
+    return num1 + num2;
+}
+

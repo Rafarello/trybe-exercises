d02e44788a87a18cc36a56e067a5c7936f764ce2
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-01-12 20:47:12 -0300

    Mockando com sinon os testes. 1h11 de aula 22.3

--- Bloco_22/Dia_3/config-loader/test/loading.js
+++ Bloco_22/Dia_3/config-loader/test/loading.js
@@ -1,27 +1,62 @@
 const loader = require('../index');
 const assert = require('assert');
 const { expect } = require('chai');
+const sinon = require('sinon');
+const fs = require('fs')
 
 describe("Carrega a configuração do arquivo",
   () => {
+
     describe("Quando o arquivo está presente",
       () => {
+
+        before(() => {
+          sinon.stub(fs, "readFileSync").returns("config")
+        })
+
         it("Retorna o conteúdo", () => {
           const config = loader('./test/fixtures/config.json');
           expect(config).to.equal('config')
+        })
+
+        after(() => {
+          fs.readFileSync.restore();
+        })
+
+      })
+
+    describe("Quando o arquivo está ausente",
+      () => {
+
+        before(() => {
+          sinon.stub(fs, "readFileSync").returns("SECRETO")
         })
 
         it("Retorna o conteúdo quando é SECRETO", () => {
           const config = loader('./test/fixtures/secret.json');
           expect(config).to.equal('SECRETO')
         })
+
+        after(() => {
+          fs.readFileSync.restore();
+        })
+
       })
 
     describe("Quando o arquivo está ausente",
       () => {
+
+        before(() => {
+          sinon.stub(fs, "readFileSync").returns("PADRÃO")
+        })
+
         it("Retorna uma configuração padrão", () => {
           const config = loader('./arquivo-que-nao-existe');
           expect(config).to.equal('PADRÃO')
+        })
+
+        after(() => {
+          fs.readFileSync.restore();
         })
       })
 

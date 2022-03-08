4a7ef683913e96ecf1c88ee460c84c34640057d0
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-03-08 15:26:14 -0300

    SRP - Exemplo BOM e teste da funcionalidade de getLetterGrades

--- Bloco_27/Dia_3/SOLID-Exercise/src/index.js
+++ Bloco_27/Dia_3/SOLID-Exercise/src/index.js
@@ -1,23 +1,35 @@
+"use strict";
 // ./src/index.ts
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.setApproved = exports.updateApprovalData = exports.approvedStudents = exports.percentageGradesIntoLetters = void 0;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getLetterGrades = exports.setApproved = exports.updateApprovalData = exports.approvedStudents = exports.percentageGradesIntoLetters = void 0;
+/* Apoio para a função `getGradeLetter` */
+const GRADE_DICT = {
+    numbers: [0.9, 0.8, 0.7, 0.6, 0.1],
+    letters: ['A', 'B', 'C', 'D', 'E'],
+};
+/* Função menor para remover o uso excessivo de `if{}else`s */
+const getGradeLetter = (gradeNumber) => {
+    const gradeNumbers = GRADE_DICT.numbers;
+    const gradeLetters = GRADE_DICT.letters;
+    for (let i = 0; i < gradeNumbers.length; i += 1) {
+        if (gradeNumber >= gradeNumbers[i])
+            return gradeLetters[i];
+    }
+    return 'F';
+};
+/* Coletar notas */
+const getLetterGrades = (discipline) => (Object.assign(Object.assign({}, discipline), { letterGrade: getGradeLetter(discipline.grade) }));
+exports.getLetterGrades = getLetterGrades;
 /* "Converter" */
-const percentageGradesIntoLetters = ({ name, disciplines }) => ({
-    name,
-    disciplines: disciplines.map(({ name, grade }) => {
-        let letterGrade;
-        if (grade >= 0.9) { letterGrade = 'A'; } else if (grade >= 0.8) { letterGrade = 'B'; } else if (grade >= 0.7) { letterGrade = 'C'; } else if (grade >= 0.6) { letterGrade = 'D'; } else if (grade >= 0.1) { letterGrade = 'E'; } else { letterGrade = 'F'; }
-        return { name, grade, letterGrade };
-    }),
-});
+const percentageGradesIntoLetters = (student) => (Object.assign(Object.assign({}, student), { disciplines: student.disciplines.map(getLetterGrades) }));
 exports.percentageGradesIntoLetters = percentageGradesIntoLetters;
 /* "Determinar" */
 const approvedStudents = ({ disciplines }) => disciplines.every(({ grade }) => grade > 0.7);
 exports.approvedStudents = approvedStudents;
 /* "Atualizar" */
-const updateApprovalData = ({ name: studentName, disciplines }) => {
-    console.log(`A pessoa com nome ${studentName} foi aprovada!`);
-    disciplines.map(({ name, letterGrade }) => console.log(`${name}: ${letterGrade}`));
+const updateApprovalData = (student) => {
+    console.log(`A pessoa com nome ${student.name} foi aprovada!`);
+    student.disciplines.forEach(({ name, letterGrade }) => console.log(`${name}: ${letterGrade}`));
 };
 exports.updateApprovalData = updateApprovalData;
 function setApproved(students) {
